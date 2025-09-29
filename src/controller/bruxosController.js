9e38a5bb59cecd90e97e39920da2335d1541dd73
Yasmin <ycrisostomo051@gmail.com>
2025-09-29 11:03:15 -0300

    delete, post e put feitos

--- src/controller/bruxosController.js
+++ src/controller/bruxosController.js
@@ -99,4 +99,80 @@ const getBruxosById = (req, res) => {
     });
 }
 
-export {getAllBruxos, getBruxosById, createBruxos};
+const deleteBruxo = (req, res) => {
+    const { id } = req.params
+
+    if (isNaN(id)) {
+        return res.status(400).json({
+            success: false,
+            message: "O id deve ser válido"
+        });
+    }
+
+const idParaApagar = parseInt(id)
+
+const BruxoParaRemover = bruxos.find(b => b.id === idParaApagar);
+
+if (!BruxoParaRemover) {
+    return res.status(404).json({
+        success: false,
+        message: "Bruxo id não existe"
+    });
+}
+const BruxoFiltrado = bruxos.filter(b => b.id !== id);
+
+bruxos.splice(0, bruxos.length, ...BruxoFiltrado);
+
+return res.status(200).json({
+    success: true,
+    message: "Bruxo expulso de Hogwarts com sucesso!"
+})
+}
+
+const updateBruxo = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nome, casa, ano, varinha, mascote, patrono, especialidade } = req.body;
+
+    if (isNaN(id)) {
+        return res.status(400).json({
+            success: false,
+            message: "O id deve ser válido"
+        });
+    }
+
+    const BruxosExiste = bruxos.find(b => b.id === id);
+
+    if (!BruxosExiste) {
+        return res.status(404).json({
+            success: false,
+            message: "Não é possível reparar o que não existe!"
+        });
+    }
+
+const BruxosAtualizados = bruxos.map(bruxos =>
+    bruxos.id === id
+        ? {
+            ...bruxos,
+            ...(nome && { nome }),
+            ...(casa && { casa }),
+            ...(ano && { ano }),
+            ...(varinha && { varinha }),
+            ...(mascote && { mascote }),
+            ...(patrono && { patrono }),
+            ...(especialidade && { especialidade }),
+        }
+        : bruxos
+);
+
+bruxos.splice(0, bruxos.length, ...BruxosAtualizados);
+
+const BruxosAtualizado = bruxos.find(b => b.id === id);
+
+res.status(200).json({
+    success: true,
+    message: "Bruxo atualizado com sucesso",
+    bruxo: BruxosAtualizado
+})
+}
+
+export {getAllBruxos, getBruxosById, createBruxos, deleteBruxo, updateBruxo};
--- src/routes/bruxosRoutes.js
+++ src/routes/bruxosRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createBruxos, getAllBruxos, getBruxosById } from "../controller/bruxosController.js";
+import { createBruxos, deleteBruxo, getAllBruxos, getBruxosById, updateBruxo } from "../controller/bruxosController.js";
 
 const router = express.Router();
 
 router.get("/", getAllBruxos);
 router.get("/:id", getBruxosById);
 router.post("/", createBruxos);
+router.delete("/:id", deleteBruxo);
+router.put("/:id", updateBruxo);
 
 export default router;
